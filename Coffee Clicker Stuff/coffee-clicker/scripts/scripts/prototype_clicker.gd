dd02564c7a66b131287cd6729e43b1e87f3a153f
mohibm708 <111067116+mohibm708@users.noreply.github.com>
2025-01-24 18:46:28 -0600

    corrections and description
    tweaked a small detail that could cause future issues, shortened some functions, added explanation of massints

---
+++ Coffee Clicker Stuff/coffee-clicker/scripts/scripts/prototype_clicker.gd
@@ -0,0 +1,163 @@
+extends Control
+
+
+# variable setup
+@export var label : Label
+var money : Massint = Massint.new([0])
+static var coffeeValue : Massint = Massint.new([1])
+var beandex : int = 1 # we begin w/ excelsa, so it's not counted in the unlock progression
+@onready var unlock : Button = get_node("Unlock_Bean")
+
+
+#>---BEAN VARS---<#
+var beans : Array
+var excelsa
+var robusta
+var arabica
+var liberica
+#>---BEAN VARS---<#
+
+
+#>---UPGRADE VARS---<#
+var upgrades : Array
+@onready var upgrade_excelsa : Button = get_node("ScrollContainer/VBoxContainer/Upgrade_Excelsa")
+@onready var upgrade_robusta : Button = get_node("ScrollContainer/VBoxContainer/Upgrade_Robusta")
+@onready var upgrade_arabica : Button = get_node("ScrollContainer/VBoxContainer/Upgrade_Arabica")
+@onready var upgrade_liberica : Button = get_node("ScrollContainer/VBoxContainer/Upgrade_Liberica")
+#>---UPGRADE VARS---<#
+
+
+#>---UPGRADE ICON VARS---<#
+var upgrade_icons : Array
+@onready var excelsa_upgrade_icon : Sprite2D = get_node("ScrollContainer/VBoxContainer/Upgrade_Excelsa/Excelsa_Upgrade_Icon")
+@onready var robusta_upgrade_icon : Sprite2D = get_node("ScrollContainer/VBoxContainer/Upgrade_Robusta/Robusta_Upgrade_Icon")
+@onready var arabica_upgrade_icon : Sprite2D = get_node("ScrollContainer/VBoxContainer/Upgrade_Arabica/Arabica_Upgrade_Icon")
+@onready var liberica_upgrade_icon : Sprite2D = get_node("ScrollContainer/VBoxContainer/Upgrade_Liberica/Liberica_Upgrade_Icon")
+#>---UPGRADE ICON VARS---<#
+
+
+#>---UPGRADE ICON SPRITES---<#
+var upgrade_sprites : Array
+@onready var excelsa_upgrade_sprite : Texture2D = preload("res://Assets/excelsa bean.png")
+@onready var robusta_upgrade_sprite : Texture2D = preload("res://Assets/robusta bean.png")
+@onready var arabica_upgrade_sprite : Texture2D = preload("res://Assets/arabaca bean.png")
+@onready var liberica_upgrade_sprite : Texture2D = preload("res://Assets/liberica bean.png")
+#>---UPGRADE ICON SPRITES---<#
+
+#>---MASSINT CHECK---<#
+func test_massint_class():
+	pass # function to test specific components of the massint class 
+#>---MASSINT CHECK---<#
+
+# calls on startup
+# sets up all the beans
+func _ready() -> void:
+	# test_massint_class()
+	Massint.new()
+	excelsa = Bean.new("Excelsa", true, 0, 1, 1, 50, 25)
+	robusta = Bean.new("Robusta", false, 500, 5, 5, 200, 100)
+	arabica = Bean.new("Arabica", false, 10000, 10, 10, 500, 250)
+	liberica = Bean.new("Liberica", false, 250000, 50, 50, 2500, 1250)
+	beans = [excelsa, robusta, arabica, liberica]
+	upgrade_icons = [excelsa_upgrade_icon, robusta_upgrade_icon, arabica_upgrade_icon, liberica_upgrade_icon]
+	upgrade_sprites = [excelsa_upgrade_sprite, robusta_upgrade_sprite, arabica_upgrade_sprite, liberica_upgrade_sprite]
+	upgrades = [upgrade_excelsa, upgrade_robusta, upgrade_arabica, upgrade_liberica]
+	update_text()
+
+# frame by frame processing
+#func _process(delta) -> void:
+#	update_coffee_value()
+#	update_money_text()
+#	update_unlock_text()
+#	update_upgrade_text()
+
+
+#>---INTERACTION FUNCTIONS---<#
+# function for when the make coffee button is pressed
+func _on_button_pressed() -> void:
+	make_coffee()
+	$Button/Coffee_Pot/AnimationPlayer.play("coffee_pot_click")
+	update_money_text()
+
+
+# functions for upgrading beans
+func _on_upgrade_excelsa_pressed() -> void:
+	upgrade_bean(excelsa)
+	update_text()
+	
+func _on_upgrade_robusta_pressed() -> void:
+	upgrade_bean(robusta)
+	update_text()
+	
+func _on_upgrade_arabica_pressed() -> void:
+	upgrade_bean(arabica)
+	update_text()
+
+func _on_upgrade_liberica_pressed() -> void:
+	upgrade_bean(liberica)
+	update_text()
+
+
+# unlocks beans
+func _on_unlock_pressed() -> void:
+	if beandex < beans.size():
+		if (money.greateq(beans[beandex].unlock_value)):
+			beans[beandex].unlocked = true
+			upgrade_icons[beandex].texture = upgrade_sprites[beandex]
+			money.subtract(beans[beandex].unlock_value)
+			beandex += 1
+			update_text()
+#>---INTERACTION FUNCTIONS---<#
+
+
+#>---MONEY FUNCTIONS---<#
+# upgrades bean sell_value if you have enough money
+func upgrade_bean(bean) -> void:
+	if bean.unlocked && money.greateq(bean.upgrade_cost):
+		money.subtract(bean.upgrade_cost)
+		bean.upgrade_bean()
+
+
+# updates your money
+func make_coffee() -> void:
+	money.add(coffeeValue)
+#>---MONEY FUNCTIONS---<#
+
+
+#>---UPDATE FUNCTIONS---<#
+func update_text() -> void:
+	update_coffee_value()
+	update_money_text()
+	update_unlock_text()
+	update_upgrade_text()
+	
+	
+# updates the value of your coffee
+func update_coffee_value() -> void:
+	coffeeValue.value = [0]
+	for bean in beans:
+		if bean.unlocked:
+			coffeeValue.add(bean.sell_value)
+
+
+# updates the amount of money displayed
+func update_money_text() -> void:
+	label.text = "$%s" % money.condensed()
+
+
+# updates unlock text
+func update_unlock_text() -> void:
+	if beandex >= beans.size():
+		unlock.text = "All beans unlocked!"
+	else:
+		var unlock_value = beans[beandex].unlock_value.condensed()
+		unlock.text = ("Unlock %s Bean\n$%s" %[beans[beandex].Name, unlock_value])
+
+
+# updates the upgrade cost displayed
+func update_upgrade_text() -> void:
+	for index in beandex:
+		var sell_value = beans[index].sell_value.condensed()
+		var upgrade_cost = beans[index].upgrade_cost.condensed()
+		upgrades[index].text = "%s\nCurrent Value: $%s\nUpgrade: $%s\nLevel: %s" %[beans[index].Name, sell_value, upgrade_cost, beans[index].level]
+#>---UPDATE FUNCTIONS---<#
